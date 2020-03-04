fee7f14416fe4041010bb37791827e738d164bb9
chuck-durst <chuckdurst67@gmail.com>
2020-03-04 16:43:35 +0100

    Add tests for the CLI

--- __test__/create.test.js
+++ __test__/create.test.js
@@ -1,24 +1,18 @@
 const assert     = require('assert');
-const spawn      = require('child_process').spawn;
+const spawn      = require('cross-spawn');
 const fileSearch = require('search-in-file').fileSearch;
 const path       = require('path');
+const fetch      = require('node-fetch');
 
 const PROJECT_NAME = 'mooglee-test-app';
-
-
-function clean(cb) {
-  const command = spawn('rm', ['-Rf', PROJECT_NAME], { cwd: path.resolve(__dirname) });
-
-  command.on('exit', () => {
-    cb();
-  });
-}
+const PORT         = 3001;
 
 function create(cb, installModules = false) {
-  spawn('rm', ['-Rf', PROJECT_NAME], { cwd: path.resolve(__dirname) });
+  spawn.sync('rm', ['-Rf', `./${PROJECT_NAME}`], { cwd: path.resolve(__dirname) });
+
   const command = spawn(
     'node',
-    [path.resolve(__dirname, '../bin/index.js'), 'create', PROJECT_NAME, '--yes'],
+    [path.resolve(__dirname, '../bin/index.js'), 'create', PROJECT_NAME, '--yes', `--port ${PORT}`],
     { cwd: path.resolve(__dirname) },
   );
 
@@ -41,12 +35,16 @@ function create(cb, installModules = false) {
 describe('CLI "create" command tests', function () {
   if (process.argv.includes('--delay')) {
     create(run, true);
-    after(clean);
   }
+
+  after(function (done) {
+    !!this.appProcess && this.appProcess.kill();
+    done();
+  });
 
   it('Check that no template variable remains', async function () {
     try {
-      const res = await fileSearch([path.resolve(__dirname, PROJECT_NAME)], /\[%.*%\]/gi, {
+      const res = await fileSearch([path.resolve(__dirname, `./${PROJECT_NAME}`)], /\[%.*%\]/gi, {
         recursive: true,
         isRegex: true,
         ignoreDir: ['node_modules'],
@@ -58,21 +56,63 @@ describe('CLI "create" command tests', function () {
   });
 
   it('Run the dev app without errors', function (done) {
-    this.appDevProcess = spawn(
+    this.appProcess = spawn(
       'npm',
       ['run', 'dev'],
-      { cwd: path.resolve(__dirname, PROJECT_NAME) },
+      { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
     );
 
-    this.appDevProcess.stderr.on('data', function (data) {
+    this.appProcess.stderr.on('data', function (data) {
       assert.fail(data);
     });
 
-    this.appDevProcess.stdout.on('data', function (data) {
+    this.appProcess.stdout.on('data', function (data) {
       if (data.indexOf('> Ready') > -1) {
         assert.ok(data);
         done();
       }
     });
   });
+
+  it('Get a 200 response when making a request to the app', async function () {
+    try {
+      const res = await fetch(`http://localhost:${PORT}`);
+
+      assert.equal(res.status, 200);
+    } catch (err) {
+      assert.fail(err.message);
+    }
+  });
+
+  it('Build the dev app without errors', function (done) {
+    const build = spawn(
+      'npm',
+      ['run', 'build'],
+      { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
+    );
+
+    build.on('exit', () => {
+      done();
+    });
+  });
+
+  it('Start the dev app without errors', function (done) {
+    this.appProcess = spawn(
+      'npm',
+      ['run', 'start'],
+      { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
+    );
+
+    this.appProcess.stderr.on('data', function (data) {
+      assert.fail(data);
+    });
+
+    this.appProcess.stdout.on('data', function (data) {
+      if (data.indexOf('> Ready') > -1) {
+        assert.ok(data);
+        done();
+      }
+    });
+  });
+
 });
