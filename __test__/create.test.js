d9a8bea72ce0f65051e4070b74045560a97b2004
chuck-durst <chuckdurst67@gmail.com>
2020-03-04 17:59:55 +0100

    Add tests for the CLI

--- __test__/create.test.js
+++ __test__/create.test.js
@@ -12,7 +12,7 @@ function create(cb, installModules = false) {
 
   const command = spawn(
     'node',
-    [path.resolve(__dirname, '../bin/index.js'), 'create', PROJECT_NAME, '--yes', `--port ${PORT}`],
+    [path.resolve(__dirname, '../bin/index.js'), 'create', PROJECT_NAME, '--yes', '--port', PORT],
     { cwd: path.resolve(__dirname) },
   );
 
@@ -31,42 +31,21 @@ function create(cb, installModules = false) {
   }
 }
 
-
-describe('CLI "create" command tests', function () {
-  if (process.argv.includes('--delay')) {
-    create(run, true);
-  }
-
-  after(function (done) {
-    !!this.appProcess && this.appProcess.kill();
-    done();
-  });
-
-  it('Check that no template variable remains', async function () {
-    try {
-      const res = await fileSearch([path.resolve(__dirname, `./${PROJECT_NAME}`)], /\[%.*%\]/gi, {
-        recursive: true,
-        isRegex: true,
-        ignoreDir: ['node_modules'],
-      });
-      assert.equal(res.length, 0);
-    } catch (err) {
-      assert.fail(err.message);
-    }
-  });
+function defaultTestsSuite() {
+  const processes = {};
 
   it('Run the dev app without errors', function (done) {
-    this.appProcess = spawn(
+    processes.dev = spawn(
       'npm',
       ['run', 'dev'],
       { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
     );
 
-    this.appProcess.stderr.on('data', function (data) {
+    processes.dev.stderr.on('data', function (data) {
       assert.fail(data);
     });
 
-    this.appProcess.stdout.on('data', function (data) {
+    processes.dev.stdout.on('data', function (data) {
       if (data.indexOf('> Ready') > -1) {
         assert.ok(data);
         done();
@@ -84,6 +63,14 @@ describe('CLI "create" command tests', function () {
     }
   });
 
+  it('Kill the dev process', function () {
+    if (typeof processes.dev.kill === 'function') {
+      processes.dev.kill();
+    } else {
+      assert.fail(`Fail to kill the dev process`);
+    }
+  });
+
   it('Build the dev app without errors', function (done) {
     const build = spawn(
       'npm',
@@ -96,18 +83,18 @@ describe('CLI "create" command tests', function () {
     });
   });
 
-  it('Start the dev app without errors', function (done) {
-    this.appProcess = spawn(
+  it('Start the production app without errors', function (done) {
+    processes.prod = spawn(
       'npm',
-      ['run', 'start'],
+      ['start'],
       { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
     );
 
-    this.appProcess.stderr.on('data', function (data) {
+    processes.prod.stderr.on('data', function (data) {
       assert.fail(data);
     });
 
-    this.appProcess.stdout.on('data', function (data) {
+    processes.prod.stdout.on('data', function (data) {
       if (data.indexOf('> Ready') > -1) {
         assert.ok(data);
         done();
@@ -115,4 +102,67 @@ describe('CLI "create" command tests', function () {
     });
   });
 
+  it('Get a 200 response when making a request to the app', async function () {
+    try {
+      const res = await fetch(`http://localhost:${PORT}`);
+
+      assert.equal(res.status, 200);
+    } catch (err) {
+      assert.fail(err.message);
+    }
+  });
+
+  it('Kill the prod process', function () {
+    if (typeof processes.prod.kill === 'function') {
+      processes.prod.kill();
+    } else {
+      assert.fail(`Fail to kill the prod process`);
+    }
+  });
+}
+
+
+describe('CLI "create" command tests', function () {
+  if (process.argv.includes('--delay')) {
+    create(run, true);
+  }
+
+  it('Check that no template variable remains', async function () {
+    try {
+      const res = await fileSearch([path.resolve(__dirname, `./${PROJECT_NAME}`)], /\[%.*%\]/gi, {
+        recursive: true,
+        isRegex: true,
+        ignoreDir: ['node_modules'],
+      });
+      assert.equal(res.length, 0);
+    } catch (err) {
+      assert.fail(err.message);
+    }
+  });
+
+  describe('Run default tests suites', function () {
+    defaultTestsSuite.call(this);
+  });
+
+  // describe('Test ejecting', function ()  {
+  //   it('Eject the app without errors', function (done) {
+  //     const command = spawn(
+  //       'npm',
+  //       ['run', 'eject'],
+  //       { cwd: path.resolve(__dirname, `./${PROJECT_NAME}`) },
+  //     );
+  //
+  //     command.stderr.on('data', function (data) {
+  //       assert.fail(data);
+  //     });
+  //
+  //     command.on('exit', function (data) {
+  //       done();
+  //     });
+  //   });
+  //
+  //   describe('Run default tests suites after ejecting', function() {
+  //     defaultTestsSuite.call(this);
+  //   });
+  // });
 });
